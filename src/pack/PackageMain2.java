3ba9cab25c14c5eec2bb416900d3897212092198
leegh1025 <leegh1025@naver.com>
2024-09-12 17:59:51 +0900

    [패키지 완료]

--- src/pack/PackageMain2.java
+++ src/pack/PackageMain2.java
@@ -6,6 +6,6 @@ public class PackageMain2 {
 
     public static void main(String[] args) {
         Data data = new Data();
-        pack.b.User user = new User(); //import 사용으로 패키지명 생략
+        pack.b.User user = new User(); //import 사용으로 패키지명 생략w
     }
 }
